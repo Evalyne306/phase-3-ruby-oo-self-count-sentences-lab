8d8584ed4845e6d651e58fc9538b319c113c60af
Evalyne306 <evalyne.wambui@student.moringaschool.com>
2023-02-14 19:26:18 +0300

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?") == true
   end
 
   def exclamation?
-
+   self.end_with?("!") == true
   end
 
   def count_sentences
-
+    sentences = self.split('.')
+    sentences.count
+    self.split(/\.|\?|!/).filter{|sentence| !sentence.empty?}.count
   end
+  
 end
+ 
